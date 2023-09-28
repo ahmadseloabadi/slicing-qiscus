cf7108397c04c44d65e9aebc8cc2715df1995dff
seloabadi <93537374+seloabadi@users.noreply.github.com>
2023-09-28 19:22:29 +0700

    first commit

---
+++ README.md
@@ -0,0 +1,1 @@
+latihan slicing web qiscus.com
---
+++ js/script.js
@@ -0,0 +1,7 @@
+// $(function () {
+//   $(".button").click(function (e) {
+//     e.preventDefault();
+//     $(".dropdownList").slideToggle(500);
+//     $(".fa-chevron-down").toggleClass("active");
+//   });
+// });
